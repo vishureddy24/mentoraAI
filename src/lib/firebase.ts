c1dacbe0507697b8327acbaa25d4252365588f36
Vishu reddy <vishureddy1224@gmail.com>
2025-09-15 09:53:01 +0000

    FirebaseError: Firebase: Error (auth/unauthorized-domain).     at create

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,16 @@
+
 import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDExU8FOTwTBsuu9uuJaFTwwce2-dMbejM',
-  authDomain: 'teenwellnessapp.firebaseapp.com',
-  projectId: 'teenwellnessapp',
-  storageBucket: 'teenwellnessapp.appspot.com',
-  messagingSenderId: '458468952403',
-  appId: '1:458468952403:web:21d28ce66913e30ec879a6',
-  measurementId: 'G-65DBWQDJ8S',
+  apiKey: "AIzaSyDExU8FOTwTBsuu9uuJaFTwwce2-dMbejM",
+  authDomain: "teenwellnessapp.firebaseapp.com",
+  projectId: "teenwellnessapp",
+  storageBucket: "teenwellnessapp.appspot.com",
+  messagingSenderId: "458468952403",
+  appId: "1:458468952403:web:21d28ce66913e30ec879a6",
+  measurementId: "G-65DBWQDJ8S"
 };
 
 // Initialize Firebase
