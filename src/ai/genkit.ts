f3f45b99dad199677f00c3c23972d972f055aefb
Vishu reddy <vishureddy1224@gmail.com>
2025-09-16 13:36:51 +0000

    Error: [GoogleGenerativeAI Error]: Error fetching from https://generativ

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,7 +3,7 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'googleai/gemini-1.5-pro-latest',
+  model: 'googleai/gemini-pro',
   safetySettings: [
     {
       category: 'HARM_CATEGORY_HATE_SPEECH',
